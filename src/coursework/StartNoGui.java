f7565689d05472e352fc6d46772bde88ff8c7e04
Kaci <kaci.yan@outlook.com>
2021-05-27 02:55:51 +0100

    Deleted parameter sets from startnogui so they're all set in the one place (parameters.java)

--- src/coursework/StartNoGui.java
+++ src/coursework/StartNoGui.java
@@ -18,13 +18,6 @@ public class StartNoGui {
 		 * 
 		 */
 
-		//Set the parameters here or directly in the Parameters Class
-		Parameters.maxEvaluations = 20000; // Used to terminate the EA after this many generations
-		Parameters.popSize = 200; // Population Size
-
-		//number of hidden nodes in the neural network
-		Parameters.setHidden(5);
-		
 		//Set the data set for training 
 		Parameters.setDataSet(DataSet.Training);
 		
