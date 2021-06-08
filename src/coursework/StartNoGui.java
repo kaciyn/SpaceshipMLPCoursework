689400fc48f45aaf10d3130d63fd6d913c246912
Kaci <kaci.yan@outlook.com>
2021-06-08 03:11:49 +0100

    Corrected the mutation rate increment to what I Actually meant it to be but it turns out I was assuming the wrong original default value so I'll recalculate it tomorrow

--- src/coursework/StartNoGui.java
+++ src/coursework/StartNoGui.java
@@ -51,34 +51,39 @@ public class StartNoGui
         
         for (int i = 1; i <= 4; i++) {
             Parameters.setResultsFileName("Crossover" + resultsSuffix);
+            createResultsFileIfNotExtant(Parameters.getResultsFilename());
             Parameters.setCrossoverType(i);
             runNeuralNet(numberOfRunsPerParameterConfiguration);
             defaultParameters();
         }
-        
+
         for (int j = 1; j <= 4; j++) {
             Parameters.setResultsFileName("ChildrenPerReproduction" + resultsSuffix);
+            createResultsFileIfNotExtant(Parameters.getResultsFilename());
             Parameters.setChildrenPerReproduction(j);
             runNeuralNet(numberOfRunsPerParameterConfiguration);
             defaultParameters();
         }
-        
+
         for (int k = 1; k <= 5; k++) {
             Parameters.setResultsFileName("GeneRange" + resultsSuffix);
+            createResultsFileIfNotExtant(Parameters.getResultsFilename());
             Parameters.setMaxGene(k);
             Parameters.setMinGene(-k);
             runNeuralNet(numberOfRunsPerParameterConfiguration);
             defaultParameters();
         }
-        
+
         for (int l = 3; l <= 7; l++) {
             Parameters.setResultsFileName("HiddenLayers" + resultsSuffix);
+            createResultsFileIfNotExtant(Parameters.getResultsFilename());
             Parameters.setHidden(l);
             runNeuralNet(numberOfRunsPerParameterConfiguration);
             defaultParameters();
         }
         for (int m = 100; m <= 1000; m += 100) {
             Parameters.setResultsFileName("PopulationSize" + resultsSuffix);
+            createResultsFileIfNotExtant(Parameters.getResultsFilename());
             Parameters.setPopSize(m);
             runNeuralNet(numberOfRunsPerParameterConfiguration);
             defaultParameters();
@@ -88,6 +93,7 @@ public class StartNoGui
         //who knows! we'll see i guess
         for (double n = 0.05; n <= 1; n *= 2) {
             Parameters.setResultsFileName("TournamentSize" + resultsSuffix);
+            createResultsFileIfNotExtant(Parameters.getResultsFilename());
             var tournamentSize = (int) (Parameters.getPopSize() * n);
             Parameters.setTournamentSize(tournamentSize);
             runNeuralNet(numberOfRunsPerParameterConfiguration);
@@ -95,18 +101,20 @@ public class StartNoGui
         }
         
         //centred around the approx. ideal mutation rate of 1/population size
-        for (double o = 0.25; o <= 2; o *= 2 * Parameters.getPopSize()) {
+        for (double o = 0.25; o <= 2; o *= 2 ) {
             Parameters.setResultsFileName("MutationRate" + resultsSuffix);
+            createResultsFileIfNotExtant(Parameters.getResultsFilename());
             var mutationToPopulationRatio = o * (1 / (double) Parameters.getPopSize());
-            
+
             Parameters.setMutateRate(mutationToPopulationRatio);
-            
+
             runNeuralNet(numberOfRunsPerParameterConfiguration);
             defaultParameters();
         }
-        
+
         for (double p = 0.01; p <= .1; p += 0.015) {
             Parameters.setResultsFileName("MutationChange" + resultsSuffix);
+            createResultsFileIfNotExtant(Parameters.getResultsFilename());
             Parameters.setMutateChange(p);
             runNeuralNet(numberOfRunsPerParameterConfiguration);
             defaultParameters();
@@ -135,7 +143,6 @@ public class StartNoGui
         Parameters.setMaxGene(3);
         
         Parameters.setPopSize(100);
-        Parameters.setMaxEvaluations(20000);
         
         Parameters.setTournamentSize(2);
         
@@ -147,7 +154,7 @@ public class StartNoGui
         
         Parameters.setChildrenPerReproduction(1);
         
-        Parameters.setMutateRate(1 / Parameters.getPopSize()); // mutation rate for mutation operator
+        Parameters.setMutateRate(1 / (double) Parameters.getPopSize()); // mutation rate for mutation operator
         Parameters.setMutateChange(Parameters.getMaxGene() - Parameters.getMinGene() / 150); // delta change for mutation operator, proportional
     }
     /**
